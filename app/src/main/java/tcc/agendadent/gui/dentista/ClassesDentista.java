86d4e5ef8f7770273b2eaf971d766d9417929969
Nathan-Dalben <nathan.flores@acad.pucrs.br>
2017-03-24 20:23:42 -0300

    Commit Add Coisinhas animacoes

---
+++ app/src/main/java/tcc/agendadent/gui/dentista/ClassesDentista.java
@@ -0,0 +1,12 @@
+package tcc.agendadent.gui.dentista;
+
+/**
+ * Created by natha on 23/03/2017.
+ */
+
+public interface ClassesDentista {
+    void onResume();
+    boolean needResume();
+    int getIdMenu();
+    void flipper(boolean next);
+}
